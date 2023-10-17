b4b63114bee9487b860b1271df43e2cf8a70d380
Markus Klassen <markusklassen1497@yahoo.de>
2023-10-17 12:34:00 +0200

    refactor

--- scripts/header.js
+++ scripts/header.js
@@ -1,19 +1,6 @@
 function loadHeader() {
-
-
-
-    var filePath = "header.html";
-
- //   var xhr = new XMLHttpRequest();
-  //  xhr.open('GET', filePath);
-
     fetch('header.html')
         .then(response => response.text())
-        .then(data => {
-            // Insert the content into the .header <div>
-            document.querySelector('.header').innerHTML = data;
-        })
+        .then(data => document.querySelector('.header').innerHTML = data)
         .catch(error => console.error('Error:', error));
-
- //   xhr.send();
 }
--- scripts/read_xml.js
+++ scripts/read_xml.js
@@ -1,34 +1,3 @@
-function loadTextFile() {
-
-    const urlParams = new URLSearchParams(window.location.search);
-
-    var filePath = urlParams.get("exercise");
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', filePath);
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var textContent = xhr.responseText;
-
-            // Parse the XML content
-            var parser = new DOMParser();
-            var xmlDoc = parser.parseFromString(textContent, 'text/xml');
-
-            // Extract the text from the "uebungtitel" node
-            var uebungTitel = xmlDoc.querySelector('exercise').textContent;
-
-            // Set the extracted text in a variable
-            var titleVariable = uebungTitel;
-
-            // Display the extracted text in the HTML element
-            document.getElementById('textid').innerHTML = titleVariable;
-        }
-    };
-
-    xhr.send();
-}
-
 function loadXMLFile() {
     const urlParams = new URLSearchParams(window.location.search);
 
@@ -40,10 +9,8 @@ function loadXMLFile() {
             var parser = new DOMParser();
             var xmlDoc = parser.parseFromString(data, 'text/xml');
 
-            // Extract the text from the "uebungtitel" node
             var uebungTitel = xmlDoc.querySelector('exercise').textContent;
 
-            // Set the extracted text in a variable
             var titleVariable = uebungTitel;
 
             // Display the extracted text in the HTML element
