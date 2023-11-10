354503990bac4d5b57b292321e114f083eeb83d4
Codekus <markusklassen1497@yahoo.de>
2023-11-11 00:12:57 +0100

    hilferuf

--- scripts/header.js
+++ scripts/header.js
@@ -3,10 +3,34 @@ async function loadHeader() {
         .then(response => response.text())
         .then(data => document.querySelector('.header').innerHTML = data)
         .catch(error => console.error('Error:', error));
+
+    fetch('mobileMenu.html')
+        .then(response => response.text())
+        .then(data => document.querySelector('#mobile-menu').innerHTML = data)
+        .catch(error => console.error('Error:', error));
 }
 
 function changeSearchbarVisibility() {
     document.getElementById("searchbar").style.setProperty("visibility", "visible");
     document.getElementById("search-icon-mobile").style.setProperty("visibility", "hidden");
     document.getElementById("search-button-mobile").style.setProperty("visibility", "hidden");
+}
+
+function openMenu() {
+    const mobileMenu = document.getElementById("mobile-menu");
+  //  document.getElementById("mobile-menu").style.display = "block";
+    mobileMenu.style.visibility = "visible"
+    mobileMenu.style.width = "100%";
+    mobileMenu.style.height = "100%"
+
+}
+
+function closeMenu() {
+  //  document.getElementById("mobile-menu").style.display = "none";
+
+    const mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu.style.visibility = "hidden"
+
+    mobileMenu.style.width = "0";
+    mobileMenu.style.height = "0"
 }
