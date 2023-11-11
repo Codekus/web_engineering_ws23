530d461f799375e289bde927cd0803f87c161d48
Markus Klassen <markusklassen1497@yahoo.de>
2023-11-11 16:00:27 +0100

    scroll is now smoother + übung page styling

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -25,12 +25,15 @@ async function loadSidebar() {
     for (var title of titles) {
         const aLink = document.createElement("a")
         aLink.setAttribute("class", "exercise-jump")
-
         aLink.innerText = title.textContent;
         aLink.onclick = function () {
+            var headerHeight = document.querySelector(".navigation").offsetHeight;
             var xpath = `//h1[text()='${this.innerText}']`;
-            const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-            element.scrollIntoView();
+            const element = document.evaluate(
+                xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null
+            ).singleNodeValue;
+            const scrollPos = element.getBoundingClientRect().top + window.scrollY - headerHeight - 30;
+            window.scrollTo({top: scrollPos, behavior: 'smooth'});
         }
         sidebarContainer.appendChild(aLink)
     }
