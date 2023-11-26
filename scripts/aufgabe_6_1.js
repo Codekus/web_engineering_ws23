7ae16643e93c65d4abd8b8f283dadc288fc06916
Codekus <markusklassen1497@yahoo.de>
2023-11-26 11:59:33 +0100

    d

--- scripts/aufgabe_6_1.js
+++ scripts/aufgabe_6_1.js
@@ -1,85 +1,74 @@
-/**
- * offene Klammer zählen und addieren -> bei geschlossener Klammer dann counter subtrahieren
- */
 
-
-function bracketCheck(str) {
-    let toCheck = "";
-    let closeStack = []
-    const symbols = [')', ']', '}'];
-    for (let i = 0; i < str.length; i++) {
-        if(!symbols.includes(str[i])) {
-            continue;
-        }
-        if (str[i] === closeStack[0]) {
-            closeStack
-            console.log("geschlossene Klammer gefunden")
-            continue;
-        }
-        if(str[i] === "(") {
-            closeStack.push(")")
-        } else if(str[i] === ")") {
-            closeStack.shift()
-            break
-        }
-    }
-}
-
-function checkString(rest, pattern) {
+function checkString(inputString, pattern) {
     let map;
-    if (pattern === undefined) {
+    if (pattern === "") {
          map = {
             "(": ")",
             "[": "]",
             "{": "}"
         }
-        rest = filterString(rest, Object.entries(map)
-            .map(([key, value]) => key + value)
-            .join(''))
     } else {
-        map = pattern
-        rest = filterString(rest, pattern)
+        map = createMapFromString(pattern)
         // regex hier rein
     }
-
     // Alles außer die Klammern entfernen
-    //let regex = /[a-zA-Z0-9]*/g;
-    //rest = rest.replaceAll(regex, "");
-    if(rest === "") {
+    inputString = filterString(inputString, Object.entries(map)
+        .map(([key, value]) => key + value)
+        .join(''))
+
+
+    if(inputString === "") {
         return true;
+    } else if (inputString.length === 1) {
+        return false
     }
 
     let stack = []
-    for(let i = 0; i < rest.length; i++) {
-        if(map.hasOwnProperty(rest[i])){
-            stack.push(rest[i])
+    for(let i = 0; i < inputString.length; i++) {
+        if(map.hasOwnProperty(inputString[i])){
+            stack.push(inputString[i])
         } else {
-            if (rest[i] === map[stack[stack.length-1]] || stack.length === 0) {
+            if (inputString[i] === map[stack[stack.length-1]]) {
                 stack.pop()
-            } else {
+            } else if (stack.length === 0) {
                 return false
             }
         }
     }
-    return true
+    return stack.length === 0
 }
 
-//bracketCheck("[(abc)]")
-//let str = "abcdef";
-//console.log(str.substring(0+1, str.length))
-console.log(checkString("(a)(b)[]{([()])}"))
-const map = {
-    "(": ")",
-    "[": "]",
-    "{": "}"
-}
-//let curr = map[rest[i]]
+/**
+ *
+ * @param inputString zb "x:y,a:b"
+ * @returns key value map ->
+ */
+function createMapFromString(inputString) {
+    const charPairs = inputString.split(',');
 
-//console.log("()([])".replace(/[)\]}]/g, ""))
+    const resultMap = {};
+    charPairs.forEach(pair => {
+        const [key, value] = pair.split(':');
+        resultMap[key] = value;
+    });
+
+    return resultMap;
+}
+
+function evaluateTextField(inputString, pattern) {
+    const isValid = checkString(inputString, pattern);
+
+    console.assert(isValid, `Validation failed the String ${inputString} is not correct`);
+
+    const inputElement = document.getElementById("input");
+    inputElement.style.background = isValid ? "green" : "red";
+}
 
 function filterString(inputString, allowedCharacters) {
-    for(inputString) // mit schleife chars entfernen
+    const allowedSet = new Set(allowedCharacters);
+
+    return inputString
+        .split('')
+        .filter(char => allowedSet.has(char))
+        .join('');
 }
-
-
-console.log(filterString("xabcyx8812y", "xy12"));
