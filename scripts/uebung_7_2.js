1887523112fe13bef6ae0022431fe9c33650c1cc
Codekus <markusklassen1497@yahoo.de>
2023-11-30 11:32:44 +0100

    uebung update

--- scripts/uebung_7_2.js
+++ scripts/uebung_7_2.js
@@ -1,14 +1,24 @@
 const stopwords = require('stopwords-de'); // array of stopwords
 const { JSDOM } = require('jsdom');
-function textAnalyzer() {
-    fetch('https://kaul.inf.h-brs.de/ccm/we/ws23/resources/assets/Plagiatsresolution.html')
-        .then(res => res.text())
-        .then(data => {
-            const dom = new JSDOM(data).window.document;
-            console.log(dom.body.textContent);
+async function textAnalyzer() {
+    const map = new Map();
 
-            // todo: filter den text: wenn wort nicht in liste, dann setze wort als key und counter als value++, dann am ende top 3 wählen
-    })
+    const text = await (await fetch('https://kaul.inf.h-brs.de/ccm/we/ws23/resources/assets/Plagiatsresolution.html')).text();
+    const bodyText = new JSDOM(text).window.document.body.textContent;
+
+
+    bodyText.split(/\s+/)
+        .map(word => word.replace(/\n/g, ""))
+        .filter(word => !stopwords.includes(word) && word.length > 0)
+        .forEach(word => {
+            if (!map.has(word)) {
+                map.set(word, 1);
+            } else {
+                map.set(word, map.get(word) + 1);
+            }
+        });
+    const sorted = [...map.entries()].sort((a, b) => b[1] - a[1]);
+    return new Map(sorted.slice(0, 3));
+
 }
-
-textAnalyzer()
+textAnalyzer().then(result => console.log(result));
