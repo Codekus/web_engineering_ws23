7b893cba795d0485abb48bd0e5900c41e5638c56
Markus Klassen <markusklassen1497@yahoo.de>
2024-02-05 13:12:11 +0100

    übung 7_2 alternative solution

--- scripts/uebung_7_2.js
+++ scripts/uebung_7_2.js
@@ -21,4 +21,30 @@ async function textAnalyzer() {
     return new Map(sorted.slice(0, 3));
 
 }
+
+// Alternative Lösung mit Map Filter Reduce, statt forEeach
+async function textAnalyzerNew() {
+    const map = new Map();
+
+    const text = await (await fetch('https://kaul.inf.h-brs.de/ccm/we/ws23/resources/assets/Plagiatsresolution.html')).text();
+    const bodyText = new JSDOM(text).window.document.body.textContent;
+
+
+    const topWords = bodyText.split(/\s+/)
+        .map(word => word.replace(/\n/g, ""))
+        .filter(word => !stopwords.includes(word) && word.length > 0)
+        .reduce((map, word) => {
+            const count = map.get(word) || 0;
+            map.set(word, count + 1);
+            return map;
+        }, new Map());
+
+    const sorted = [...topWords.entries()].sort((a, b) => b[1] - a[1]);
+    return new Map(sorted.slice(0, 3));
+
+
+}
 textAnalyzer().then(result => console.log(result));
+textAnalyzerNew().then(result => console.log(result));
+
+
